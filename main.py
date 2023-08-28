d3d7bbcf94557a434b225dc9b0d7527f78afc52c
Mihailo Pesic <pesic.mihailo@jjzmaj.edu.rs>
2023-08-28 17:06:18 +0200

    napisao sam

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-import numpy as np
-import cv2 as cv
-import easygui
+from gui import GUI
+import tkinter as tk
 
-face_cascade = cv.CascadeClassifier('face.xml')
-def todo():
-    img = easygui.fileopenbox()
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 7, 7)
-    for (x,y,w,h) in faces:
-        cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
+def main():
+    root = tk.Tk()
 
+    app = GUI(root)
 
-        cv.imshow('img',img)
-        cv.waitKey(0)
-        cv.destroyAllWindows()
+    root.mainloop()
 
-todo()
+if __name__ == "__main__":
+    main()
