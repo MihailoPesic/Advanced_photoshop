ad9cd6b8b89dd3495e94aa2a8dcb910613427176
Mihailo Pesic <pesic.mihailo@jjzmaj.edu.rs>
2023-08-28 19:14:51 +0200

    tu krecemo svi

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from gui import GUI
 import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk, ImageEnhance
+
+def promeni_kontast(slika, vrednost):
+    ImageEnhance.Contrast(slika).enchance(vrednost)
+
+def promeni_osvetljenje(slika, vrednost):
+    ImageEnhance.Brightness(slika).enhance(vrednost)
+
+def promeni_boju(slika, vrednost):
+    ImageEnhance.Color(slika).enhance(vrednost)
+
+def promeni_ostrinu(slika, vrednost):
+    ImageEnhance.Sharpness(slika).enhance(vrednost)
 
 def main():
     root = tk.Tk()
@@ -10,3 +24,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
